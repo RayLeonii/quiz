3786aec285c791735119cd39b4f35efd920b2b3f
Gabriela Cisneros <Gabriela.Cisneros@calbaptist.edu>
2025-09-25 20:16:49 -0700

    dark them

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -5,6 +5,14 @@ import 'package:quiz/questions_screen.dart';
 import 'package:quiz/data/questions.dart';
 import 'package:quiz/results_screen.dart';
 
+var kColorScheme =
+    ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 96, 59, 181));
+
+var kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: Color.fromARGB(255, 5, 99, 125),
+);
+
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
 
@@ -37,6 +45,7 @@ class _QuizState extends State<Quiz> {
   void restartQuiz() {
     setState(() {
       _activeScreen = 'questions-screen';
+      _selectedAnswers = []; 
     });
   }
 
@@ -46,8 +55,8 @@ class _QuizState extends State<Quiz> {
 
     if (_activeScreen == 'questions-screen') {
       screenWidget = QuestionsScreen(
-        onSelectAnswer: _chooseAnswer,
-      );
+        onSelectAnswer: _chooseAnswer
+        );
     }
 
     if (_activeScreen == 'results-screen') {
@@ -58,20 +67,40 @@ class _QuizState extends State<Quiz> {
     }
 
     return MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 78, 13, 151),
-                Color.fromARGB(255, 107, 15, 168),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+      ),
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: kDarkColorScheme,
+      ),
+      themeMode: ThemeMode.system,
+      home: Builder(
+        builder: (context) {
+          final brightness = Theme.of(context).brightness;
+          final isDark = brightness == Brightness.dark;
+
+          return Scaffold(
+            body: Container(
+              decoration: BoxDecoration(
+                gradient: isDark
+                    ? const LinearGradient(
+                        colors: [Colors.black, Colors.black], 
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                      )
+                    : const LinearGradient(
+                        colors: [
+                          Color.fromARGB(255, 81, 22, 181), 
+                          Color.fromARGB(255, 114, 92, 151),
+                        ],
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                      ),
+              ),
+              child: screenWidget,
             ),
-          ),
-          child: screenWidget,
-        ),
+          );
+        },
       ),
     );
   }
